117ae37147cf9508609d4f56e237f394fe705eda
m-ahrukh <72295010+m-ahrukh@users.noreply.github.com>
2024-01-14 11:46:05 +0500

    updted UI

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,38 +1,38 @@
 import dbConnect, { pool } from "@/utils/dbConnect";
 import { redirect } from "next/navigation";
 
-export default async function Edit({params}) {
+export default async function Edit({ params }) {
 
   dbConnect()
   let id = params.id
 
-  const data = await pool.query("select * from todo where id = $1 ",[id])
-    const result = data.rows[0]
+  const data = await pool.query("select * from todo where id = $1 ", [id])
+  const result = data.rows[0]
 
-  async function editTask(data){
+  async function editTask(data) {
     'use server'
     let note = data.get('note').valueOf()
 
-    try{
-      const updatedTask = await pool.query("update todo set text = $1 where id = $2 returning *", [note, id])
+    try {
+      const updatedTask = await pool.query("UPDATE todo SET text = $1 WHERE id = $2 RETURNING *", [note, id])
       console.log(updatedTask.rows[0])
     }
-    catch(err){
+    catch (err) {
       console.log("Error ", err)
     }
     redirect('/')
   }
- 
+
   return (
     <div>
-      <div className='m-5'>
-        <h1 className='text-center m-5'>
+      <div className='m-5 justify-center items-center'>
+        <h1 className='text-center mb-3 mx-5 font-semibold'>
           Edit Task
         </h1>
-        <form action={editTask} className='space-y-5'>
+        <form action={editTask} className='flex flex-col justify-center items-center'>
           <input type='text' name='note' id='note' placeholder='Add Note'
             defaultValue={result.text}
-            className='shadow-lg rounded-md shadow-black h-10 p-3 w-[100%]' />
+            className='shadow-lg rounded-md shadow-black h-10 p-3 w-[100%] mb-6' />
           <button type='submit' className='bg-orange-500 font-bold text-white hover:bg-red-600 p-3 rounded-md'>SUBMIT</button>
         </form>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import dbConnect, {pool} from '@/utils/dbConnect'
+import dbConnect, { pool } from '@/utils/dbConnect'
 import { redirect } from 'next/navigation'
 import Link from 'next/link'
 
@@ -6,60 +6,59 @@ export default async function Home() {
 
   dbConnect()
 
-  const data = await pool.query("select * from todo")
+  const data = await pool.query("sELECT * FROM todo")
   const result = data.rows
 
   //Insert Query
-  async function addTask(data){
+  async function addTask(data) {
     'use server'
     let note = data.get("note")?.valueOf();
-    let tasksLength  = result.length
+    let tasksLength = result.length
 
-    try{
-      const newNote = await pool.query("insert into todo(id, text) values ($1, $2) returning *", [tasksLength+1, note])
+    try {
+      const newNote = await pool.query("INSERT INTRO todo(id, text) VALUES ($1, $2) RETURNING *", [tasksLength + 1, note])
       console.log(newNote.rows[0])
     }
-    catch(err){
+    catch (err) {
       console.log("Error ", err)
     }
     redirect('/')
   }
 
-  async function deleteTask(data){
+  async function deleteTask(data) {
     'use server'
     let id = data.get('id').valueOf()
-
-    try{
-      const taskToBeDeleted = await pool.query('delete from todo where id = $1',[id])
+    try {
+      const taskToBeDeleted = await pool.query('DELETE FROM todo WHERE id = $1', [id])
     }
-    catch(err){
+    catch (err) {
       console.log("Error ", err)
     }
     redirect('/')
   }
 
   return (
-    <main className='m-10'>
-      <div className='m-5'>
-        <h1 className='text-center m-5'>
+    <main className=' mt-10 flex flex-col justify-center items-center'>
+      <h1 className='font-bold' style={{ fontSize: '24px' }}>To Do Application</h1>
+      <div className='mb-3 mx-5'>
+        <h1 className='text-center m-5 font-semibold'>
           Add Task
         </h1>
-        <form action={addTask} className='space-y-5'>
+        <form action={addTask} className='flex flex-col justify-center items-center'>
           <input type='text' name='note' id='note' placeholder='Add Note'
-          className='shadow-lg rounded-md shadow-black h-10 p-3 w-[100%]' />
+            className='shadow-lg rounded-md shadow-black h-10 p-3 mb-6' />
           <button type='submit' className='bg-orange-500 font-bold text-white hover:bg-red-600 p-3 rounded-md'>SUBMIT</button>
         </form>
       </div>
       {
-        result.map((dataRow)=> {
+        result.map((dataRow) => {
           return (
-            <div className='flex flex-row'>
-              <div className='flex flex-row gap-5 w-1/2'>
-              <p>{dataRow.id}:</p>
-              <p>{dataRow.text}</p>
+            <div className='flex flex-row mt-4'>
+              <div className='flex flex-row gap-5 w-1/2 my-4'>
+                <p>{dataRow.text}</p>
               </div>
-              <div className='w-1/2 flex flex-row gap-5'>
-                <Link href={'/edit/'+dataRow.id}>
+              <div className='w-1/2 flex flex-row gap-5 my-2'>
+                <Link href={'/edit/' + dataRow.id}>
                   <button className='bg-cyan-600 font-bold text-white p-2'>UPDATE</button>
                 </Link>
                 <form action={deleteTask}>
